ee6de5cc4e3d81667c894750ab71278c29934b25
Montblanc <montmoguri@gmail.com>
2025-09-17 03:34:21 -0700

    Renamed latin_short_frlg_* to latin_frlg_*

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -230,10 +230,10 @@ $(FONTGFXDIR)/small.latfont: $(FONTGFXDIR)/latin_small.png
 	$(GFX) $< $@
 
 # $(FONTGFXDIR)/normal.latfont: $(FONTGFXDIR)/latin_normal.png
-$(FONTGFXDIR)/normal.latfont: $(FONTGFXDIR)/latin_short_frlg_nums.png
+$(FONTGFXDIR)/normal.latfont: $(FONTGFXDIR)/latin_frlg_nums.png
 	$(GFX) $< $@
 
-$(FONTGFXDIR)/short.latfont: $(FONTGFXDIR)/latin_short_frlg_nums.png
+$(FONTGFXDIR)/short.latfont: $(FONTGFXDIR)/latin_frlg_nums.png
 	$(GFX) $< $@
 
 $(FONTGFXDIR)/narrow.latfont: $(FONTGFXDIR)/latin_narrow.png
