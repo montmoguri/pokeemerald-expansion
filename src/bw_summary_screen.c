795bf3243200d705a892aa06567e3ef427e28acb
Montblanc <montmoguri@gmail.com>
2025-09-21 22:55:48 -0700

    Moved PrintColoredStatLabel outside, moved next lv exp print location, more stats column spacings

--- src/bw_summary_screen.c
+++ src/bw_summary_screen.c
@@ -398,6 +398,7 @@ static const u8 sText_ViewStats[]                           = _("View Stats");
 static const u8 sText_ViewIVs_Graded[]                      = _("See Innate");
 static const u8 sText_ViewEVs_Graded[]                      = _("See Effort");
 static const u8 sText_NextLv[]                              = _("Next {LV}");
+static const u8 sText_Next[]                                = _("Next");
 static const u8 sText_RentalPkmn[]                          = _("Rental Pokémon");
 static const u8 sText_None[]                                = _("None");
 #else
@@ -415,6 +416,7 @@ static const u8 sText_ViewStats[]                           = _("VIEW STATS");
 static const u8 sText_ViewIVs_Graded[]                      = _("SEE INNATE");
 static const u8 sText_ViewEVs_Graded[]                      = _("SEE EFFORT");
 static const u8 sText_NextLv[]                              = _("NEXT {LV}");
+static const u8 sText_Next[]                                = _("NEXT");
 static const u8 sText_RentalPkmn[]                          = _("RENTAL POKéMON");
 static const u8 sText_None[]                                = _("NONE");
 #endif
@@ -574,8 +576,8 @@ static const struct WindowTemplate sSummaryTemplate[] =
     },
     [PSS_LABEL_WINDOW_POKEMON_SKILLS_EXP] = {
         .bg = 0,
-        .tilemapLeft = 5,
-        .tilemapTop = 10,
+        .tilemapLeft = 6,
+        .tilemapTop = 11,
         .width = 11,
         .height = 4,
         .paletteNum = 6,
@@ -669,13 +671,13 @@ static const struct WindowTemplate sPageSkillsTemplate[] =
         .paletteNum = 6,
         .baseBlock = 335,
     },
-    // this was modify to remove HP bar and just print value together with non-HP stats
+    // this was modified to remove HP bar and just print value together with non-HP stats
     [PSS_DATA_WINDOW_SKILLS_STATS] = {
         .bg = 0,
         .tilemapLeft = 1,
-        .tilemapTop = 4,   // Moved down 1
-        .width = 17,
-        .height = 6,       // Increased from 2 to 9 to also print non-HP stats
+        .tilemapTop = 5,
+        .width = 18,
+        .height = 6,
         .paletteNum = 6,
         .baseBlock = 355,
     },
@@ -691,9 +693,9 @@ static const struct WindowTemplate sPageSkillsTemplate[] =
     },
     [PSS_DATA_WINDOW_EXP_NEXT_LEVEL] = {
         .bg = 0,
-        .tilemapLeft = 10,
-        .tilemapTop = 10,
-        .width = 9,
+        .tilemapLeft = 15,
+        .tilemapTop = 11,
+        .width = 4,
         .height = 2,
         .paletteNum = 6,
         .baseBlock = 473,
@@ -3508,8 +3510,8 @@ static void HandleAppealJamTilemap(u16 move)
 // }
 
 // Move up by 3 tiles (24 pixels) AND left by 8 tiles (64 pixels)
-#define EXP_BAR_VERTICAL_OFFSET_TILES -3
-#define EXP_BAR_HORIZONTAL_OFFSET_TILES -14  // Negative = left, positive = right
+#define EXP_BAR_VERTICAL_OFFSET_TILES -2
+#define EXP_BAR_HORIZONTAL_OFFSET_TILES -9  // Negative = left, positive = right
 #define EXP_BAR_TILEMAP_START (0x1F4 + (EXP_BAR_VERTICAL_OFFSET_TILES * 32) + EXP_BAR_HORIZONTAL_OFFSET_TILES)
 #define EXP_BAR_TILE_EMPTY    0x2100
 #define EXP_BAR_TILE_FULL     0x2108
@@ -3739,7 +3741,8 @@ static void PrintPageNamesAndStats(void)
         PrintTextOnWindow(PSS_LABEL_WINDOW_PROMPT_STATS, sText_ViewStats, stringXPos, 1, 0, 1);
     }
 
-    PrintTextOnWindow(PSS_LABEL_WINDOW_POKEMON_SKILLS_EXP, sText_NextLv, 0, 0, 0, 0);
+    PrintTextOnWindowWithFont(PSS_LABEL_WINDOW_POKEMON_SKILLS_EXP, sText_Next, 36, 0, 0, 0, FONT_SMALL);
+    PrintTextOnWindowWithFont(PSS_LABEL_WINDOW_POKEMON_SKILLS_EXP, gText_LevelSymbol, 60, 0, 0, 0, FONT_NORMAL);
 }
 
 static void PutPageWindowTilemaps(u8 page)
@@ -4302,38 +4305,11 @@ static void UNUSED PrintRibbonCount(void)
     PrintTextOnWindow(AddWindowFromTemplateList(sPageSkillsTemplate, PSS_DATA_WINDOW_SKILLS_RIBBON_COUNT), text, x, 1, 0, 0);
 }
 
-static void BufferStat(u8 *dst, s8 statIndex, u32 stat, u32 strId, u32 align)
+
+static void BufferStat(u8 *dst, u32 stat, u32 strId, u32 align)
 {
-    static const u8 sTextNatureDown[] = _("{COLOR}{08}");
-    static const u8 sTextNatureUp[] = _("{COLOR}{05}");
-    static const u8 sTextNatureNeutral[] = _("{COLOR}{01}");
-    static const u8 sTextUpArrow[] = _(" {UP_ARROW}");
-    static const u8 sTextDownArrow[] = _(" {DOWN_ARROW}");
-    u8 *txtPtr;
-
-    if (statIndex == 0 
-        || !BW_SUMMARY_NATURE_COLORS 
-        || gNaturesInfo[sMonSummaryScreen->summary.mintNature].statUp == gNaturesInfo[sMonSummaryScreen->summary.mintNature].statDown)
-        txtPtr = StringCopy(dst, sTextNatureNeutral);
-    else if (statIndex == gNaturesInfo[sMonSummaryScreen->summary.mintNature].statUp)
-        txtPtr = StringCopy(dst, sTextNatureUp);
-    else if (statIndex == gNaturesInfo[sMonSummaryScreen->summary.mintNature].statDown)
-        txtPtr = StringCopy(dst, sTextNatureDown);
-    else
-        txtPtr = StringCopy(dst, sTextNatureNeutral);
-
-    ConvertIntToDecimalStringN(txtPtr, stat, STR_CONV_MODE_RIGHT_ALIGN, align);
-
-    if (statIndex != 0 
-        && BW_SUMMARY_NATURE_ARROWS 
-        && gNaturesInfo[sMonSummaryScreen->summary.mintNature].statUp != gNaturesInfo[sMonSummaryScreen->summary.mintNature].statDown)
-    {
-        if (statIndex == gNaturesInfo[sMonSummaryScreen->summary.mintNature].statUp)
-            StringAppend(txtPtr, sTextUpArrow);
-        else if (statIndex == gNaturesInfo[sMonSummaryScreen->summary.mintNature].statDown)
-            StringAppend(txtPtr, sTextDownArrow);
-    }
-
+    // Simplified BufferStat that only converts to string, no nature coloring
+    ConvertIntToDecimalStringN(dst, stat, STR_CONV_MODE_RIGHT_ALIGN, align);
     DynamicPlaceholderTextUtil_SetPlaceholderPtr(strId, dst);
 }
 
@@ -4388,26 +4364,26 @@ static void BufferAndPrintStats_HandleState(u8 mode)
 
         DynamicPlaceholderTextUtil_Reset();
         // Pass 0 as statIndex to disable nature coloring on numbers
-        BufferStat(gStringVar1, 0, atk, 0, 3);     // was STAT_ATK
-        BufferStat(gStringVar2, 0, def, 1, 3);     // was STAT_DEF  
-        BufferStat(gStringVar3, 0, spA, 2, 3);     // was STAT_SPATK
-        BufferStat(gStringVar4, 0, spD, 3, 3);     // was STAT_SPDEF
-        BufferStat(sStringVar5, 0, spe, 4, 3);     // was STAT_SPEED
+        BufferStat(gStringVar1, atk, 0, 3);
+        BufferStat(gStringVar2, def, 1, 3);
+        BufferStat(gStringVar3, spA, 2, 3);
+        BufferStat(gStringVar4, spD, 3, 3);
+        BufferStat(sStringVar5, spe, 4, 3);
         PrintNonHPStats();
     }
     else
     {
-        BufferStat(maxHPString, 0, hp, 0, 7);
+        BufferStat(maxHPString, hp, 0, 7);
         DynamicPlaceholderTextUtil_Reset();
         DynamicPlaceholderTextUtil_SetPlaceholderPtr(0, maxHPString);
         DynamicPlaceholderTextUtil_ExpandPlaceholders(gStringVar4, sStatsHPIVEVLayout);
         PrintHPStats(mode);
 
-        BufferStat(gStringVar1, 0, atk, 0, 3);
-        BufferStat(gStringVar2, 0, def, 1, 3);
-        BufferStat(gStringVar3, 0, spA, 2, 3);
-        BufferStat(gStringVar4, 0, spD, 3, 3);
-        BufferStat(sStringVar5, 0, spe, 4, 3);
+        BufferStat(gStringVar1, atk, 0, 3);
+        BufferStat(gStringVar2, def, 1, 3);
+        BufferStat(gStringVar3, spA, 2, 3);
+        BufferStat(gStringVar4, spD, 3, 3);
+        BufferStat(sStringVar5, spe, 4, 3);
         PrintNonHPStats();
     }
 
@@ -4435,27 +4411,39 @@ static void BufferHPStats(void)
 static void PrintHPStats(u8 mode)
 {
     u8 windowId = AddWindowFromTemplateList(sPageSkillsTemplate, PSS_DATA_WINDOW_SKILLS_STATS);
-    PrintTextOnWindowWithFont(windowId, gText_HP_Title, 0, 0, 0, 0, FONT_SMALL);
+    PrintTextOnWindowWithFont(windowId, gText_HP_Title, 4, 0, 0, 0, FONT_SMALL);
     if (mode == SKILL_STATE_STATS)
-        PrintTextOnWindow(windowId, gStringVar4, 24, 0, 0, 0);
+        PrintTextOnWindow(windowId, gStringVar4, 28, 0, 0, 0);
     else
-        PrintTextOnWindow(windowId, gStringVar4, 24, 0, 0, 0);
+        PrintTextOnWindow(windowId, gStringVar4, 28, 0, 0, 0);
 }
 
 
 static void BufferNonHPStats(void)
 {
     DynamicPlaceholderTextUtil_Reset();
-    BufferStat(gStringVar1, 0, sMonSummaryScreen->summary.atk, 0, 3);
-    BufferStat(gStringVar2, 0, sMonSummaryScreen->summary.def, 1, 3);
-    BufferStat(gStringVar3, 0, sMonSummaryScreen->summary.spatk, 2, 3);
-    BufferStat(gStringVar4, 0, sMonSummaryScreen->summary.spdef, 3, 3);
-    BufferStat(sStringVar5, 0, sMonSummaryScreen->summary.speed, 4, 3);
-    // BufferStat(gStringVar1, STAT_ATK, sMonSummaryScreen->summary.atk, 0, 3);
-    // BufferStat(gStringVar2, STAT_DEF, sMonSummaryScreen->summary.def, 1, 3);
-    // BufferStat(gStringVar3, STAT_SPATK, sMonSummaryScreen->summary.spatk, 2, 3);
-    // BufferStat(gStringVar4, STAT_SPDEF, sMonSummaryScreen->summary.spdef, 3, 3);
-    // BufferStat(sStringVar5, STAT_SPEED, sMonSummaryScreen->summary.speed, 4, 3);
+    BufferStat(gStringVar1, sMonSummaryScreen->summary.atk, 0, 3);
+    BufferStat(gStringVar2, sMonSummaryScreen->summary.def, 1, 3);
+    BufferStat(gStringVar3, sMonSummaryScreen->summary.spatk, 2, 3);
+    BufferStat(gStringVar4, sMonSummaryScreen->summary.spdef, 3, 3);
+    BufferStat(sStringVar5, sMonSummaryScreen->summary.speed, 4, 3);
+}
+
+
+static void PrintColoredStatLabel(u8 windowId, s8 statIndex, const u8 *text, u8 x, u8 y, 
+                                  u8 natureUpStat, u8 natureDownStat, u8 *coloredLabel)
+{
+    static const u8 sTextNatureDown[] = _("{COLOR}{08}");
+    static const u8 sTextNatureUp[] = _("{COLOR}{05}");
+    static const u8 sTextNatureNeutral[] = _("{COLOR}{01}");
+    
+    const u8 *color = (statIndex == natureUpStat) ? sTextNatureUp : 
+                     (statIndex == natureDownStat) ? sTextNatureDown : 
+                     sTextNatureNeutral;
+    
+    StringCopy(coloredLabel, color);
+    StringAppend(coloredLabel, text);
+    PrintTextOnWindowWithFont(windowId, coloredLabel, x, y, 0, 0, FONT_SMALL);
 }
 
 static void PrintNonHPStats(void)
@@ -4463,47 +4451,23 @@ static void PrintNonHPStats(void)
     u8 windowId = AddWindowFromTemplateList(sPageSkillsTemplate, PSS_DATA_WINDOW_SKILLS_STATS);
     u8 coloredLabel[64];
     
-    static const u8 sTextNatureDown[] = _("{COLOR}{08}");
-    static const u8 sTextNatureUp[] = _("{COLOR}{05}");
-    static const u8 sTextNatureNeutral[] = _("{COLOR}{01}");
-    
     // Pre-calculate nature info once
-    const u8 *neutralColor = sTextNatureNeutral;
-    const u8 *upColor = sTextNatureUp;
-    const u8 *downColor = sTextNatureDown;
-    u8 natureUpStat = 0;
-    u8 natureDownStat = 0;
-    
-    if (BW_SUMMARY_NATURE_COLORS) 
-    {
-        natureUpStat = gNaturesInfo[sMonSummaryScreen->summary.mintNature].statUp;
-        natureDownStat = gNaturesInfo[sMonSummaryScreen->summary.mintNature].statDown;
-    }
-    
-    // Optimized helper - just lookup, no calculations
-    void printColoredStat(s8 statIndex, const u8 *text, u8 x, u8 y) {
-        const u8 *color = (statIndex == natureUpStat) ? upColor : 
-                         (statIndex == natureDownStat) ? downColor : 
-                         neutralColor;
-        
-        StringCopy(coloredLabel, color);
-        StringAppend(coloredLabel, text);
-        PrintTextOnWindowWithFont(windowId, coloredLabel, x, y, 0, 0, FONT_SMALL);
-    }
+    u8 natureUpStat = gNaturesInfo[sMonSummaryScreen->summary.mintNature].statUp;
+    u8 natureDownStat = gNaturesInfo[sMonSummaryScreen->summary.mintNature].statDown;
 
-    // Print all stats
-    printColoredStat(STAT_ATK, gText_Attack_Title, 72, 0);
-    printColoredStat(STAT_DEF, gText_Defense_Title, 0, 16);
-    printColoredStat(STAT_SPATK, gText_SpAtk_Title, 72, 16);
-    printColoredStat(STAT_SPDEF, gText_SpDef_Title, 0, 32);
-    printColoredStat(STAT_SPEED, gText_Speed_Title, 72, 32);
+    // Print non-HP stat labels (colored)
+    PrintColoredStatLabel(windowId, STAT_ATK, gText_Attack_Title, 80, 0, natureUpStat, natureDownStat, coloredLabel);
+    PrintColoredStatLabel(windowId, STAT_DEF, gText_Defense_Title, 4, 16, natureUpStat, natureDownStat, coloredLabel);
+    PrintColoredStatLabel(windowId, STAT_SPATK, gText_SpAtk_Title, 80, 16, natureUpStat, natureDownStat, coloredLabel);
+    PrintColoredStatLabel(windowId, STAT_SPDEF, gText_SpDef_Title, 4, 32, natureUpStat, natureDownStat, coloredLabel);
+    PrintColoredStatLabel(windowId, STAT_SPEED, gText_Speed_Title, 80, 32, natureUpStat, natureDownStat, coloredLabel);
 
-    // Print numbers (unchanged)
-    PrintTextOnWindow(windowId, gStringVar1, 112, 0, 0, 0);
-    PrintTextOnWindow(windowId, gStringVar2, 48, 16, 0, 0);
-    PrintTextOnWindow(windowId, gStringVar3, 112, 16, 0, 0);
-    PrintTextOnWindow(windowId, gStringVar4, 48, 32, 0, 0);
-    PrintTextOnWindow(windowId, sStringVar5, 112, 32, 0, 0);
+    // Print non-HP stat values (no color)
+    PrintTextOnWindow(windowId, gStringVar1, 126, 0, 0, 0);
+    PrintTextOnWindow(windowId, gStringVar2, 52, 16, 0, 0);
+    PrintTextOnWindow(windowId, gStringVar3, 126, 16, 0, 0);
+    PrintTextOnWindow(windowId, gStringVar4, 52, 32, 0, 0);
+    PrintTextOnWindow(windowId, sStringVar5, 126, 32, 0, 0);
 }
 
 static void PrintExpPointsNextLevel(void)
@@ -4524,9 +4488,9 @@ static void PrintExpPointsNextLevel(void)
     else
         expToNextLevel = 0;
 
-    ConvertIntToDecimalStringN(gStringVar1, expToNextLevel, STR_CONV_MODE_RIGHT_ALIGN, 6);
+    ConvertIntToDecimalStringN(gStringVar1, expToNextLevel, STR_CONV_MODE_RIGHT_ALIGN, 5);
 
-    PrintTextOnWindow(windowIdNextLvl, gStringVar1, 1, 0, 0, 0);
+    PrintTextOnWindow(windowIdNextLvl, gStringVar1, 2, 0, 0, 0);
     // PrintTextOnWindowWithFont(windowIdNextLvl, gStringVar1, 1, 4, 0, 0, FONT_NARROW);
 }
 
